baf1f208333b8a22faa9d76b0a7184b2be106431
Angelo Aroni <angeduff@hotmail.com>
2022-11-15 10:18:05 +0100

    ultimo commit cambio de tsx a jsx

---
+++ src/components/Error.jsx
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled'
+
+const Texto = styled.div`
+  background-color: #b7322c;
+  color: #fff;
+  padding: 15px;
+  font-size: 15px;
+  text-transform: uppercase;
+  font-family: 'Lat', sans-serif;
+  font-weight: 700;
+  text-align: center;
+`
+
+const Error = ({ children }) => {
+  return <Texto>{children}</Texto>
+}
+
+export default Error
